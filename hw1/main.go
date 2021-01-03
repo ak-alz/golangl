55c3973ac365176ad45c52cce05a06f64e3147e2
AK-VoronM <ak.alz@mail.ru>
2021-01-03 23:02:29 +0300

    time output

--- hw1/main.go
+++ hw1/main.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 	response, err := ntp.Query("0.beevik-ntp.pool.ntp.org")
-	time := time.Now().Add(response.ClockOffset)
 
 	if err == nil {
+		time := time.Now().Add(response.ClockOffset)
 		fmt.Println(time)
 	} else {
 		fmt.Println("Ни тута")
